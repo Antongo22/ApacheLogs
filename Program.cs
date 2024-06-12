1369b1aca8d3c793d086b38a20ebaebb5b7be7f0
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2024-06-12 20:55:32 +0300

    работает без крона

--- Program.cs
+++ Program.cs
@@ -1,40 +1,135 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ApacheLogs
 {
     internal class Program
     {
-        static List<LogEntry> logs;
+        static string configpath = "config.txt";
+
         static void Main(string[] args)
         {
-            DataBase.Create();
-            Console.WriteLine("Database created successfully.");
+            Console.WriteLine("Добро пожаловать в программу по просмотрк логов. Вот список доступных команд\n" +
+                              "openconfig - открывает в редакторе по умолчанию файл конфига\n" +
+                              "close - завершает выполнение программы\n" +
+                              "parse - получает данные из конфига, сопоставляет их с логими. Полученные данные из логов записывает в базу данных\n" +
+                              "getlog (date|datefrom) (dateto) (ip) (status) - получает данные логов из уже выгруженной базе данных.");
 
-            Parse();
+            while (true)
+            {
+                Console.Write("> ");
 
-            bool isSuc = DataBase.SetDatas(logs);
-            Console.WriteLine("Log inserted successfully.");
+                string command = Console.ReadLine();
 
-            DataBase.GetLogsByFilter(DateTime.Parse("28/Jul/2006"), DateTime.Parse("10/Aug/2006"), null, null);
+                switch (command.ToLower().Trim())
+                {
+                    case "parse":
+                        Parse(configpath);
+                        break;
+                    case "openconfig":
+                        OpenFileInDefaultProgram(configpath);
+                        break;
+                    case var s when s.StartsWith("getlog"):
+                        GetData(s);
+                        break;
+                    case "close":
+                        return;
+                    default:
+                        Console.WriteLine("Неизвестная команда!");
+                        break;
+                }
+            }
         }
 
-
-        static void Parse()
+        static void OpenFileInDefaultProgram(string filePath)
         {
-            List<LogEntry> res = Apache.Parse("config.txt");
+            try
+            {
+                Process.Start(filePath);
+                Console.WriteLine("Не забудьте сохранить файл и прописать команду parse, если это требуется!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при открытии файла: " + ex.Message);
+            }
+        }
+
+        static void Parse(string configpath)
+        {
+            List<LogEntry> res = Apache.Parse(configpath);
             if(res == null)
             {
                 Console.WriteLine("Не удалось считать днные");
             }
             else
             {
-                logs = res;
+                SetDateBase(res);
+                Console.WriteLine("Данные успешно получены!");
             }
+        }
+
+        static void SetDateBase(List<LogEntry> logs)
+        {
+            DataBase.Create();
+            bool isSuc = DataBase.SetDatas(logs);
+        }
+
+        static void GetData(string commandline)
+        {
+            string[] commands = commandline.Trim().Split(' ');
+
+            Console.WriteLine(commands.Length);
+
+            DateTime? dateFrom = null;
+            DateTime? dateTo = null;
+            string ip = null;
+            int? status = null;
+
+
+            for (int i = 1; i < commands.Length; i++)
+            {
+                string datetmp = commands[i];
+                DateTime date;
+
+                Console.WriteLine(datetmp);
+                if (datetmp.Length == 3 && int.TryParse(datetmp, out int sta))
+                {
+                    status = sta;
+                }
+                else if (datetmp.Count(c => c == '.') == 3)
+                {
+                    ip = datetmp;
+                }
+                else if (DateTime.TryParseExact(datetmp, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date) ||
+                        DateTime.TryParseExact(datetmp, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date) ||
+                        DateTime.TryParseExact(datetmp, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date) ||
+                        DateTime.TryParseExact(datetmp, "dd/MMM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime))
+                {
+                    if (dateFrom == null)
+                    {
+                        dateFrom = date;
+                    }
+                    else
+                    {
+                        dateTo = date;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат данных!4");
+                    return;
+                }
+            }
+               
+            DataBase.GetLogsByFilter(dateFrom, dateTo, ip, status);          
         }
     }
 }
